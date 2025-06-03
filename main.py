d531cbe85e9fc90a6b783227a2e758d26017413f
oldgreyduck <caquelin@hey.com>
2025-06-03 19:00:28 +0000

    made changes to align with course solutions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -11,15 +12,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+
         player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,9 +1,15 @@
-from circleshape import *
+import pygame
+from constants import *
+from circleshape import CircleShape
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -12,13 +18,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    
-    def draw(self, screen):
-            pygame.draw.polygon(screen, "white", self.triangle(), width = 2)
-
-    def rotate(self, dt):
-        self.rotation += (PLAYER_TURN_SPEED * dt)
-        print(self.rotation)
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -27,3 +26,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(self.rotation)
