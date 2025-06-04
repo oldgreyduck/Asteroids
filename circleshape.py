3a3753c5681821b4c173a0c6b6f0f898fcd1a157
oldgreyduck <caquelin@hey.com>
2025-06-04 18:29:54 +0000

    adding final solutions

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collision(self,other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+
